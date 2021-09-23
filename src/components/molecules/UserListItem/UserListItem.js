d50e61ae1fcde4917255c8e63cd335c805ca3f8d
Cnoters <sadamowski2@gmail.com>
2021-09-23 16:27:33 +0200

    route params for userlist fetching

--- src/components/molecules/UserListItem/UserListItem.js
+++ src/components/molecules/UserListItem/UserListItem.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper } from './UserListItem.styles';
 import { DeleteButton as Button } from 'components/atoms/DeleteButton/DeleteButton';
 import { Average } from 'components/atoms/Average/Average';
 import { Attendance } from 'components/atoms/Attendance/Attendance';
+import { UsersContext } from 'providers/UsersProvider';
 
-export const UserListItem = ({ deleteUser, index, user: { average, name, attendance } }) => (
-  <Wrapper>
-    <Average average={average} />
-    <Attendance user={{ name, attendance }} />
-    <Button onClick={() => deleteUser(name)} />
-  </Wrapper>
-);
+export const UserListItem = ({ index, user: { average, name, attendance } }) => {
+  const { deleteUser } = useContext(UsersContext);
+
+  return (
+    <Wrapper>
+      <Average average={average} />
+      <Attendance user={{ name, attendance }} />
+      <Button onClick={() => deleteUser(name)} />
+    </Wrapper>
+  );
+};
 
 UserListItem.propTypes = {
   userData: PropTypes.shape({
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,5 +1,40 @@
 import UsersList from 'components/organisms/UsersList/UsersList';
+import { ViewWrapper } from 'components/atoms/ViewWrapper/ViewWrapper';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { NavLink as Link } from 'react-router-dom';
+import axios from 'axios';
 
 export const Dashboard = () => {
-  return <UsersList />;
+  const [students, setStudents] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const { group } = useParams();
+  console.log(group || groups[0], group, groups[0]);
+
+  useEffect(() => {
+    axios
+      .get(`/groups`)
+      .then(({ data }) => setGroups(data.groups))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`/students/${group || groups[0]}`)
+      .then(({ data }) => setStudents(data.students))
+      .catch((err) => console.log(err));
+  }, [group, groups]);
+
+  return (
+    <ViewWrapper>
+      <nav>
+        {groups.map((group) => (
+          <Link to={`/group/${group}`} key={group}>
+            {group}
+          </Link>
+        ))}
+      </nav>
+      <UsersList users={students} />
+    </ViewWrapper>
+  );
 };
