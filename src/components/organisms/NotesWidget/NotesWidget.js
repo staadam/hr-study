c45911bbe42b148671284aa82bd49e6cb99e1a22
Cnoters <sadamowski2@gmail.com>
2021-10-09 20:41:48 +0200

    add note from form

--- src/components/organisms/NotesWidget/NotesWidget.js
+++ src/components/organisms/NotesWidget/NotesWidget.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Note } from '../../molecules/Note/Note';
 import { Wrapper, WidgetHandler, NotesWrapper } from './NotesWidget.styled';
 import { useSelector } from 'react-redux';
 
 export const NotesWidget = () => {
   const notes = useSelector((state) => state.notes);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenWidget = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
-    <Wrapper isOpen>
-      <WidgetHandler>notes</WidgetHandler>
+    <Wrapper isOpen={isOpen}>
+      <WidgetHandler onClick={handleOpenWidget}>notes</WidgetHandler>
       <NotesWrapper>
         {notes.length ? notes.map(({ title, content, id }) => <Note key={id} title={title} content={content} id={id} />) : <p>No notes so far</p>}
       </NotesWrapper>
--- src/views/Notes.js
+++ src/views/Notes.js
@@ -3,22 +3,33 @@ import { Wrapper, FormWrapper, StyledFormField, NotesWrapper } from './Notes.sty
 import { Button } from 'components/atoms/Button/Button';
 import { Note } from 'components/molecules/Note/Note';
 import { useSelector, useDispatch } from 'react-redux';
-import { addNote, removeNote } from 'store/store';
+import { addNote } from 'store/store';
+import { useForm } from 'react-hook-form';
 
 export const Notes = () => {
   const notes = useSelector((state) => state.notes);
   const dispatch = useDispatch();
 
-  const handleAddNote = () => {
-    dispatch(addNote({ title: `New note ${Math.floor(Math.random() * 100)}`, content: 'Lorem ipsum dolor sit amet' }));
+  const handleAddNote = ({ title, content }) => {
+    dispatch(addNote({ title, content }));
+    reset({ title: '', content: '' });
   };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   return (
     <Wrapper>
       <FormWrapper>
-        <StyledFormField label="Title" name="title" id="title" />
-        <StyledFormField isTextarea label="Notes" name="notes" id="notes" />
-        <Button onClick={handleAddNote}>Add</Button>
+        <StyledFormField label="Title" name="title" id="title" {...register('title', { required: true })} />
+        {errors.title ? 'Title is required' : null}
+        <StyledFormField isTextarea label="Notes" name="notes" id="notes" {...register('content', { required: true })} />
+        {errors.content ? 'Content of the note is required' : null}
+        <Button onClick={handleSubmit(handleAddNote)}>Add</Button>
       </FormWrapper>
       <NotesWrapper>
         {notes.length ? notes.map(({ id, title, content }) => <Note key={id} title={title} content={content} id={id} />) : <p>No notes so far</p>}
