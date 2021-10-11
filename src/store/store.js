1a0639957d2b35348579b137be1f0b67314a91fa
Cnoters <sadamowski2@gmail.com>
2021-10-11 13:43:51 +0200

    redux toolkit query

--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,28 @@
-import { createStore } from 'redux';
 import { v4 as uuid } from 'uuid';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-export const addNote = (payload) => {
-  return {
-    type: 'notes/add',
-    payload: {
-      id: uuid(),
-      ...payload,
+const initialNotesState = [{ id: uuid(), title: 'Lorem ipsum', content: 'Lorem ispum dolor sit amet.' }];
+
+const notesSlice = createSlice({
+  name: 'notes',
+  initialState: initialNotesState,
+  reducers: {
+    addNote(state, action) {
+      state.push({
+        id: uuid(),
+        ...action.payload,
+      });
     },
-  };
-};
+    removeNote(state, action) {
+      return state.filter((note) => note.id !== action.payload.id);
+    },
+  },
+});
 
-export const removeNote = (payload) => {
-  return {
-    type: 'notes/remove',
-    payload,
-  };
-};
+export const { addNote, removeNote } = notesSlice.actions;
 
-const initialState = {
-  notes: [{ id: uuid(), title: 'Lorem ipsum', content: 'Lorem ispum dolor sit amet.' }],
-};
-
-const notesReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'notes/add':
-      return { ...state, notes: [...state.notes, action.payload] };
-    case 'notes/remove':
-      return {
-        notes: state.notes.filter((note) => note.id !== action.payload.id),
-      };
-    default:
-      return state;
-  }
-};
-
-export const store = createStore(notesReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = configureStore({
+  reducer: {
+    notes: notesSlice.reducer,
+  },
+});
